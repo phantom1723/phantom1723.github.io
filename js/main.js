455b9e94673fa2aefb067cfb3ed0f071df93037d
phantom23 <andruhin2@ukr.net>
2018-07-01 21:38:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-/**
- * Created by User on 12.01.2017.
- *
- */
-/*
-require('./menu');
-require('./changeHeroBackground');
-require('./totoroWalk');
-require('./timer');
-require('./carousel');
-require('./scroller');*/
-
-
 import showMenu from './menu';
-import changeHeroBackground from './changeHeroBackground';
 import totoroWalk from './totoroWalk';
 import timer from './timer';
 import carousel from './carousel';
